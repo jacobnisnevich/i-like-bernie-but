e506e874b97b8ffc201f1b36db355004692e903b
Alex Nisnevich <alex.nisnevich@gmail.com>
2016-01-26 11:11:39 -0800

    Writing some questions

--- js/faq.js
+++ js/faq.js
@@ -1,25 +1,28 @@
 var faq = [{
-    "question": "...isn't he a socialist?",
+    "question": "... isn't he a socialist?",
     "answer": "Blah... Denmark... blah...democratic socialist... blah..."
-}, {
-    "question": "...isn't he too weak on gun control?",
+},{
+    "question": "... I heard he wants to raise taxes!",
     "answer": "Blah... D- NRA record... blah..."
 },{
-    "question": "...isn't he a socialist?",
+    "question": "... is he even electable?",
     "answer": "Blah... Denmark... blah...democratic socialist... blah..."
-}, {
-    "question": "...isn't he too weak on gun control?",
+},{
+    "question": "... isn't he too weak on gun control?",
     "answer": "Blah... D- NRA record... blah..."
 },{
-    "question": "...isn't he a socialist?",
+    "question": "... how could he get anything done with a Republican Congress?",
     "answer": "Blah... Denmark... blah...democratic socialist... blah..."
-}, {
-    "question": "...isn't he too weak on gun control?",
+},{
+    "question": "... isn't he too weak on gun control?",
     "answer": "Blah... D- NRA record... blah..."
 },{
-    "question": "...isn't he a socialist?",
+    "question": "... I heard he wants to get rid of Obamacare!",
     "answer": "Blah... Denmark... blah...democratic socialist... blah..."
-}, {
-    "question": "...isn't he too weak on gun control?",
+},{
+    "question": "... does he have any experience with foreign policy?",
+    "answer": "Blah... D- NRA record... blah..."
+},{
+    "question": "... aren't all his supporters young white liberals?",
     "answer": "Blah... D- NRA record... blah..."
 }];
