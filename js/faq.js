192f040169f2ee270dca6c3d2e99e673c18bb57f
Alex Nisnevich <alex.nisnevich@gmail.com>
2016-01-27 00:58:31 -0800

    first draft of taxes answer

--- js/faq.js
+++ js/faq.js
@@ -5,7 +5,9 @@ var faq = [{
         "But this isn't radical or scary: many of the programs instituted by Franklin D. Roosevelt and Lyndon Johnson that we take for granted today &mdash; such as Medicare, the 40-hour work week, and the minimum wage &mdash; <a href='http://www.theatlantic.com/politics/archive/2015/11/bernie-sanders-makes-his-pitch-for-socialism/416913/'>can be considered socialist programs</a> ."
 },{
     "question": "... I heard he wants to raise taxes!",
-    "answer": "Blah... D- NRA record... blah..."
+    "answer": 
+        "This is technically true, but misleading. If you're paying $5,000 a year for health insurance today and you're in the middle class, Bernie's healthcare plan <a href='http://berniecare.org/'>will save you $2000&mdash;$4000 per year</a> . Imagine never having to pay insurance premiums again!<br><br>" +
+        "Bernie's tax plan <i>will</i> increase taxes on the very wealthy, but your marginal tax rate will only go up if you're making more than $231,450 a year, and the tax hike will only be significant if you're making more than $2,000,000 a year. <a href='http://datatitian.com/bernie-tax/'>Most Americans will see significant savings</a> ."
 },{
     "question": "... is he even electable?",
     "answer": "Blah... Denmark... blah...democratic socialist... blah..."
