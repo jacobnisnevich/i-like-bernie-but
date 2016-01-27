a7662ddf44cce367c6442712729870d9308d4fcf
Alex Nisnevich <alex.nisnevich@gmail.com>
2016-01-27 01:22:41 -0800

    rough draft of electability question

--- js/faq.js
+++ js/faq.js
@@ -2,18 +2,19 @@ var faq = [{
     "question": "... isn't he a socialist?",
     "answer": 
         "Bernie Sanders is a <a href='http://www.politifact.com/truth-o-meter/article/2015/aug/26/bernie-sanders-socialist-or-democratic-socialist/'><i>democratic socialist</i></a> . In essence, he believes that our current economic system doesn't do enough to help poor and middle-class Americans and that democratic change is needed to create a more fair and just system.<br><br>" + 
-        "But this isn't radical or scary: many of the programs instituted by Franklin D. Roosevelt and Lyndon Johnson that we take for granted today &mdash; such as Medicare, the 40-hour work week, and the minimum wage &mdash; <a href='http://www.theatlantic.com/politics/archive/2015/11/bernie-sanders-makes-his-pitch-for-socialism/416913/'>can be considered socialist programs</a> ."
+        "But this isn't radical or scary! Many of the programs instituted by Franklin D. Roosevelt and Lyndon Johnson that we take for granted today &mdash; such as Medicare, the 40-hour work week, and the minimum wage &mdash; <a href='http://www.theatlantic.com/politics/archive/2015/11/bernie-sanders-makes-his-pitch-for-socialism/416913/'>can be considered socialist programs</a> ."
 },{
     "question": "... I heard he wants to raise taxes!",
     "answer": 
-        "This is technically true, but misleading. If you're paying $5,000 a year for health insurance today and you're in the middle class, Bernie's healthcare plan <a href='http://berniecare.org/'>will save you $2000&mdash;$4000 per year</a> . Imagine never having to pay insurance premiums again!<br><br>" +
+        "This is technically true, but misleading. If you're paying $5,000 a year for health insurance today and you're in the middle class, Bernie's healthcare plan will save you $2000&mdash;$4000 per year. <a href='http://berniecare.org/'>[Try the calculator]</a> Imagine never having to pay insurance premiums again!<br><br>" +
         "Bernie's tax plan <i>will</i> increase taxes on the very wealthy, but your marginal tax rate will only go up if you're making more than $231,450 a year, and the tax hike will only be significant if you're making more than $2,000,000 a year. <a href='http://datatitian.com/bernie-tax/'>Most Americans will see significant savings</a> ."
 },{
     "question": "... is he even electable?",
-    "answer": "Blah... Denmark... blah...democratic socialist... blah..."
-},{
-    "question": "... isn't he too weak on gun control?",
-    "answer": "Blah... D- NRA record... blah..."
+    "answer": 
+        "Absolutely. Bernie probably has a better chance in the general election than Hillary would have:<br><br>" +
+        "Bernie performs better than Hillary does in all hypothetical match-ups against Republicans in <a href='http://www.realclearpolitics.com/epolls/2016/president/2016_presidential_race.html'>poll after poll</a> (2.4% better against Trump, 4.6% better against Cruz, and 1.5% better against Rubio, on average).<br><br>" +
+        "Bernie significantly outperforms Hillary in <a href='http://ivn.us/2015/09/15/ivn-presidential-poll-bernie-sanders-donald-trump-ben-carson-lead/'>surveys of <i>independent voters</i></a>, and with 30-40% of Americans identifying as independents, they will play the deciding role in the general election.<br><br>" +
+        "And Bernie has a big lead in favorability, with a <a href='http://elections.huffingtonpost.com/pollster/bernie-sanders-favorable-rating'><b>+10%</b> net favorability</a> rating among all Americans, compared to <a href='http://elections.huffingtonpost.com/pollster/hillary-clinton-favorable-rating'>Hillary's <b>-10%</b> net favorability rating</a>. <b>No</b> presidential candidate has <a href='https://lh6.googleusercontent.com/-bBLIXEsM56U/VoRD8zsEUlI/AAAAAAABxjs/CAnZZGXpO7M/w874-h894-no/OIusBY8.jpg'><b>ever</b> won with a negative favorability rating</a>."
 },{
     "question": "... how could he get anything done with a Republican Congress?",
     "answer": "Blah... Denmark... blah...democratic socialist... blah..."
