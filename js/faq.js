1ebaf88a9ffafbcb2e1ca1dc7cb7f2b48b420f74
Alex Nisnevich <alex.nisnevich@gmail.com>
2016-01-27 00:47:23 -0800

    first draft of socialism answer

--- js/faq.js
+++ js/faq.js
@@ -1,6 +1,8 @@
 var faq = [{
     "question": "... isn't he a socialist?",
-    "answer": "Blah... Denmark... blah...democratic socialist... blah..."
+    "answer": 
+        "Bernie Sanders is a <a href='http://www.politifact.com/truth-o-meter/article/2015/aug/26/bernie-sanders-socialist-or-democratic-socialist/'><i>democratic socialist</i></a> . In essence, he believes that our current economic system doesn't do enough to help poor and middle-class Americans and that democratic change is needed to create a more fair and just system.<br><br>" + 
+        "But this isn't radical or scary: many of the programs instituted by Franklin D. Roosevelt and Lyndon Johnson that we take for granted today &mdash; such as Medicare, the 40-hour work week, and the minimum wage &mdash; <a href='http://www.theatlantic.com/politics/archive/2015/11/bernie-sanders-makes-his-pitch-for-socialism/416913/'>can be considered socialist programs</a> ."
 },{
     "question": "... I heard he wants to raise taxes!",
     "answer": "Blah... D- NRA record... blah..."
