f6ffa81d95d5540b56c1475ca2aba1e8b4ffa8cc
Alex Nisnevich <alex.nisnevich@gmail.com>
2016-01-27 02:10:16 -0800

    rough drafts of congress and guns questions

--- js/faq.js
+++ js/faq.js
@@ -17,10 +17,14 @@ var faq = [{
         "And Bernie has a big lead in favorability, with a <a href='http://elections.huffingtonpost.com/pollster/bernie-sanders-favorable-rating'><b>+10%</b> net favorability</a> rating among all Americans, compared to <a href='http://elections.huffingtonpost.com/pollster/hillary-clinton-favorable-rating'>Hillary's <b>-10%</b> net favorability rating</a>. <b>No</b> presidential candidate has <a href='https://lh6.googleusercontent.com/-bBLIXEsM56U/VoRD8zsEUlI/AAAAAAABxjs/CAnZZGXpO7M/w874-h894-no/OIusBY8.jpg'><b>ever</b> won with a negative favorability rating</a>."
 },{
     "question": "... how could he get anything done with a Republican Congress?",
-    "answer": "Blah... Denmark... blah...democratic socialist... blah..."
+    "answer": 
+        "Bernie is actually well-known for his ability to compromise to get things done without sacrificing his values. In the House, he was known as the Amendment King, and passed more amendments &mdash; <a href='http://www.dailykos.com/story/2015/10/6/1428616/-Bernie-Sanders-What-the-Hell-Have-You-Done-for-Us-Lately'>addressing exclusively progressive goals</a> &mdash; <a href='http://www.alternet.org/election-2016/bernie-gets-it-done-sanders-record-pushing-through-major-reforms-will-surprise-you'>than any other legislator</a>, by forging cross-party coalitions.<br><br>" + 
+        "He has earned respect from Republicans ranging from <a href='http://www.washingtonexaminer.com/mccain-calls-sanders-x-rated-but-an-honest-man/article/2577874'>John McCain</a> to the ultra-conservative <a href='http://www.bloomberg.com/features/2016-bernie-sanders-fundraising/'>Jim Inhofe</a>. If any Democratic president can reach across the aisle to work with a stubborn Republican Congress, it's Bernie Sanders."
 },{
     "question": "... isn't he too weak on gun control?",
-    "answer": "Blah... D- NRA record... blah..."
+    "answer": 
+        "Hardly &mdash; he has a <a href='http://votesmart.org/candidate/evaluations/27110/bernie-sanders/37'>25% lifetime score</a> from the NRA and <a href='http://votesmart.org/candidate/evaluations/27110/bernie-sanders/37'>consistent high ratings</a> from the Brady Campaign to Prevent Gun Violence.<br><br>" +
+        "His position is that <a href='http://thehill.com/blogs/blog-briefing-room/255741-sanders-sees-consensus-for-serious-gun-control'>a broad consensus of common-sense gun control measures</a> is possible if neither side is alienated through overly divisive rhetoric."
 },{
     "question": "... I heard he wants to get rid of Obamacare!",
     "answer": "Blah... Denmark... blah...democratic socialist... blah..."
