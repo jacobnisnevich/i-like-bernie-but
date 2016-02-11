d3cfe0f9bc3f212ae3fae72b6244aedc16dd69db
Alex Nisnevich <alex.nisnevich@gmail.com>
2016-02-10 23:52:33 -0800

    Florida registration message

--- js/messages.js
+++ js/messages.js
@@ -20,6 +20,13 @@ var messages = {
       text: "<b>Monday, February 15</b> is your deadline to register to vote! You can be registered under any party to vote in the Democratic primary, but just make sure that you're registered by Monday.<div class='small'>[<a href='https://myinfo.alabamavotes.gov/VoterView/RegistrantSearch.do'>Check if you're registered</a>]&nbsp;&nbsp;&nbsp;<b>[<a href='https://www.alabamavotes.gov/olvr/default.aspx'>Register online!</a>]</b>&nbsp;&nbsp;&nbsp;[<a href='http://voteforbernie.org/state/alabama/'>More info</a>]</div>"
     }
   ],
+  "Florida": [
+    {
+      startTime: new Date('February 9, 2016 23:59:59 GMT-0600'),
+      deadline: new Date('February 16, 2016 23:59:59 GMT-0600'),
+      text: "<b>Monday, February 16</b> is your deadline to register to vote! In Florida, you <b>must be registered as a Democrat</b> to vote in the Democratic primary. Florida doesn't have online registration, but you can <a href='http://voteforbernie.org/register-to-vote/'>fill out a registration form</a> to print and mail in.<div class='small'>[<a href='http://registration.elections.myflorida.com/CheckVoterStatus'>Check your voter status</a>]&nbsp;&nbsp;&nbsp;[<a href='http://voteforbernie.org/state/florida/'>More info</a>]</div>"
+    }
+  ],
   "Ohio": [
     {
       startTime: new Date('February 13, 2016 23:59:59 GMT-0600'),
