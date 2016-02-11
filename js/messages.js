7499ca3d599713e686dc071113b8b468640f3a96
Alex Nisnevich <alex.nisnevich@gmail.com>
2016-02-10 23:57:02 -0800

    Missouri registration message

--- js/messages.js
+++ js/messages.js
@@ -17,21 +17,28 @@ var messages = {
     {
       startTime: new Date('February 12, 2016 23:59:59 GMT-0500'),
       deadline: new Date('February 15, 2016 23:59:59 GMT-0500'),
-      text: "<b>Monday, February 15</b> is your deadline to register to vote! You can be registered under any party to vote in the Democratic primary, but just make sure that you're registered by Monday.<div class='small'>[<a href='https://myinfo.alabamavotes.gov/VoterView/RegistrantSearch.do'>Check if you're registered</a>]&nbsp;&nbsp;&nbsp;<b>[<a href='https://www.alabamavotes.gov/olvr/default.aspx'>Register online!</a>]</b>&nbsp;&nbsp;&nbsp;[<a href='http://voteforbernie.org/state/alabama/'>More info</a>]</div>"
+      text: "<b>Tuesday, February 15</b> is your deadline to register to vote! You can be registered under any party to vote in the Democratic primary, but just make sure that you're registered by Monday.<div class='small'>[<a href='https://myinfo.alabamavotes.gov/VoterView/RegistrantSearch.do'>Check if you're registered</a>]&nbsp;&nbsp;&nbsp;<b>[<a href='https://www.alabamavotes.gov/olvr/default.aspx'>Register online!</a>]</b>&nbsp;&nbsp;&nbsp;[<a href='http://voteforbernie.org/state/alabama/'>More info</a>]</div>"
     }
   ],
   "Florida": [
     {
-      startTime: new Date('February 13, 2016 23:59:59 GMT-0600'),
-      deadline: new Date('February 16, 2016 23:59:59 GMT-0600'),
-      text: "<b>Monday, February 16</b> is your deadline to register to vote! In Florida, you <b>must be registered as a Democrat</b> to vote in the Democratic primary. Florida doesn't have online registration, but you can <a href='http://voteforbernie.org/register-to-vote/'>fill out a registration form</a> to print and mail in.<div class='small'>[<a href='http://registration.elections.myflorida.com/CheckVoterStatus'>Check your voter status</a>]&nbsp;&nbsp;&nbsp;[<a href='http://voteforbernie.org/state/florida/'>More info</a>]</div>"
+      startTime: new Date('February 13, 2016 23:59:59 GMT-0500'),
+      deadline: new Date('February 16, 2016 23:59:59 GMT-0500'),
+      text: "<b>Tuesday, February 16</b> is your deadline to register to vote! In Florida, you <b>must be registered as a Democrat</b> to vote in the Democratic primary. Florida doesn't have online registration, but you can <a href='http://voteforbernie.org/register-to-vote/'>fill out a registration form</a> to print and mail in.<div class='small'>[<a href='http://registration.elections.myflorida.com/CheckVoterStatus'>Check your voter status</a>]&nbsp;&nbsp;&nbsp;[<a href='http://voteforbernie.org/state/florida/'>More info</a>]</div>"
     }
   ],
   "Ohio": [
     {
       startTime: new Date('February 13, 2016 23:59:59 GMT-0600'),
       deadline: new Date('February 16, 2016 23:59:59 GMT-0600'),
-      text: "<b>Monday, February 16</b> is your deadline to register to vote! Ohio does not have online registration, but you can <a href='http://voteforbernie.org/register-to-vote/'>fill out a registration form</a> to print and mail in.<div class='small'>[<a href='http://voterlookup.sos.state.oh.us/voterlookup.aspx'>Check if you're registered</a>]&nbsp;&nbsp;&nbsp;[<a href='http://voteforbernie.org/state/ohio/'>More info</a>]</div>"
+      text: "<b>Tuesday, February 16</b> is your deadline to register to vote! Ohio does not have online registration, but you can <a href='http://voteforbernie.org/register-to-vote/'>fill out a registration form</a> to print and mail in.<div class='small'>[<a href='http://voterlookup.sos.state.oh.us/voterlookup.aspx'>Check if you're registered</a>]&nbsp;&nbsp;&nbsp;[<a href='http://voteforbernie.org/state/ohio/'>More info</a>]</div>"
+    }
+  ],
+  "Missouri": [
+    {
+      startTime: new Date('February 14, 2016 23:59:59 GMT-0600'),
+      deadline: new Date('February 17, 2016 23:59:59 GMT-0600'),
+      text: "<b>Wednesday, February 17</b> is your deadline to <a href='http://www.sos.mo.gov/elections/goVoteMissouri/register'>register to vote</a>! <div class='small'>[<a href='https://s1.sos.mo.gov/elections/voterlookup/'>Check your voter status</a>]&nbsp;&nbsp;&nbsp;[<a href='http://voteforbernie.org/state/missouri/'>More info</a>]</div>"
     }
   ]
 }
