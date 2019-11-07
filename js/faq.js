eac04dca6c1dcd6fc4a839f9c16fca953b804ea7
Alex Nisnevich <alex.nisnevich@gmail.com>
2019-11-07 01:29:56 -0800

    starting work on 2020 edition

--- js/faq.js
+++ js/faq.js
@@ -1,34 +1,36 @@
 var faq = [{
     "id": "what-about-socialism",
     "question": "... isn't he a socialist?",
-    "answer": 
-        "Bernie Sanders is a <a href='http://www.politifact.com/truth-o-meter/article/2015/aug/26/bernie-sanders-socialist-or-democratic-socialist/'><i>democratic socialist</i></a>. He believes that our current economic system isn't doing enough for poor and middle-class Americans and that democratic change is needed to create a more fair and just America.<br><br>" + 
-        "But this isn't radical or scary! Many of the programs instituted by Franklin D. Roosevelt and Lyndon Johnson that we take for granted today &mdash; such as Medicare, the 40-hour work week, the minimum wage, and Social Security &mdash; <a href='http://www.theatlantic.com/politics/archive/2015/11/bernie-sanders-makes-his-pitch-for-socialism/416913/'>can be considered socialist programs</a>."
+    "answer":
+        "Bernie Sanders is a <i>democratic socialist</i>. In <a href='https://edition.cnn.com/videos/politics/2019/04/23/bernie-sanders-town-hall-failures-of-socialism-question-soviet-russia-sot-vpx.cnn'>his own words</a>: &ldquo;What democratic socialism means to me is we expand Medicare, we provide educational opportunities for all Americans, we rebuild our crumbling infrastructure. In other words, Government serves the needs of all people rather than just wealthy campaign contributors.&rdquo;<br><br>" +
+        "This isn't radical or scary! Many of the programs instituted by Franklin D. Roosevelt and Lyndon Johnson that we take for granted today &mdash; such as Medicare, the 40-hour work week, the minimum wage, and Social Security &mdash; <a href='http://www.theatlantic.com/politics/archive/2015/11/bernie-sanders-makes-his-pitch-for-socialism/416913/'>can be considered socialist programs</a>.<br><br>" +
+        "In 2019, Bernie delivered <a href='https://mobile.twitter.com/BernieSanders/status/1138868379979173888'>a major speech on democratic socialism</a> and why his message is what America needs to defeat Donald Trump."
 },{
     "id": "isnt-he-raising-taxes",
     "question": "... I heard he wants to raise taxes!",
-    "answer": 
+    "answer":
         "If you're one of the 1.5% of Americans making more than $231,450 a year, your <i>marginal</i> tax rate will go up slightly &mdash; money you earn above $231,450 will be taxed at 37% instead of 33%.<br><br>" +
-        "If you're one of the 0.6% of Americans making more than $500,000 a year, your marginal rate will go up from 39.6% to 43%. Above $2,000,000 the rate will be 48%, and above $10,000,000 the rate will be 52%. These are tax increases that will only affect the very, <i>very</i> wealthy.<br><br>" +
-        "On the other hand, the vast majority of Americans <a href='http://datatitian.com/bernie-tax/'>will see significant savings</a> when factoring in tax and healthcare changes under Bernie's plans."
+        "If you're one of the 0.6% of Americans making more than $500,000 a year, your marginal rate will go up from 39.6% to 45%. Above $2,000,000 the rate will be 50%, and above $10,000,000 the rate will be 52%. These are tax increases that will only affect the very, <i>very</i> wealthy.<br><br>" +
+        "On the other hand, the vast majority of Americans <a href='http://datatitian.com/bernie-tax/'>will see significant savings</a> when factoring in tax and healthcare changes under Bernie's plans. <a href='https://www.bernietax.com/'>Calculate how Bernie's tax plans will affect you.</a>"
 },{
     "id": "is-he-electable",
     "question": "... is he even electable?",
-    "answer": 
-        "Absolutely. Bernie probably has a better chance in the general election than Hillary would have:<br><br>" +
-        "Bernie performs better than Hillary does in all hypothetical match-ups against Republicans in <a href='http://www.realclearpolitics.com/epolls/2016/president/2016_presidential_race.html'>poll after poll</a>, and on average does 7.4% better against Trump (Sanders +11.8% against Trump compared to Clinton +6.2% against Trump).<br><br>" +
-        "Bernie significantly outperforms Hillary in <a href='http://ivn.us/2015/09/15/ivn-presidential-poll-bernie-sanders-donald-trump-ben-carson-lead/'>surveys of <i>independent voters</i></a>, and with 30-40% of Americans identifying as independents, they will play the deciding role in the general election.<br><br>" +
-        "And Bernie has a big lead in favorability, with a <a href='http://elections.huffingtonpost.com/pollster/bernie-sanders-favorable-rating'><b>+11%</b> net favorability</a> rating among all Americans, compared to <a href='http://elections.huffingtonpost.com/pollster/hillary-clinton-favorable-rating'>Hillary's <b>-13%</b> net favorability rating</a>. <b>No</b> presidential candidate has <a href='https://lh6.googleusercontent.com/-bBLIXEsM56U/VoRD8zsEUlI/AAAAAAABxjs/CAnZZGXpO7M/w874-h894-no/OIusBY8.jpg'><b>ever</b> won with a negative favorability rating</a>."
+    "answer":
+        "Absolutely. Bernie probably has a better chance in the general election than any other candidate because he appeals to working families, young people, and the 30-40% of Americans identifying as independents, who will play the deciding role in the general election.<br><br>" +
+        "Almost every <a href='https://www.realclearpolitics.com/epolls/2020/president/us/general_election_trump_vs_sanders-6250.html'>major poll in this election</a> shows Bernie beating Trump, often by very large margins. In 2016, Bernie <a href='https://en.wikipedia.org/wiki/Results_of_the_2016_Democratic_Party_presidential_primaries'>won 23 state primaries</a>. And Bernie has a high favorability rating among all Americans. People trust Bernie. And most importantly, Bernie has a <a href='https://feelthebern.org/who-is-bernie-sanders/'>consistent lifetime record</a> of being right on the issues, then and now. Plus:<br><br>" +
+        "&bull; Bernie has the ground game to win. Bernie’s solid base of supporters, volunteers and donors are unmatched by any other candidate.<br><br>" +
+        "&bull; <a href='https://www.thedailybeast.com/bernie-sanders-raises-massive-dollar253-million-in-third-quarter'>Bernie has the money to win.</a> Over one million people have already donated to Bernie’s 2020 campaign.<br><br>" +
+        "&bull; Bernie has a strategy to win by reaching out to young people, independents, disaffected voters, non-voters and working-class voters who have given up on the political process."
 },{
     "id": "can-he-get-stuff-done",
     "question": "... how could he get anything done with a Republican Congress?",
-    "answer": 
-        "Bernie is actually well-known for his ability to compromise to get things done without sacrificing his values. In the House, he was known as the Amendment King, and passed more amendments, <a href='http://www.dailykos.com/story/2015/10/6/1428616/-Bernie-Sanders-What-the-Hell-Have-You-Done-for-Us-Lately'>addressing exclusively progressive goals</a>, <a href='http://www.alternet.org/election-2016/bernie-gets-it-done-sanders-record-pushing-through-major-reforms-will-surprise-you'>than any other legislator</a>, by forging cross-party coalitions.<br><br>" + 
+    "answer":
+        "Bernie is actually well-known for his ability to compromise to get things done without sacrificing his values. In the House, he was known as the Amendment King, and passed more amendments, <a href='http://www.dailykos.com/story/2015/10/6/1428616/-Bernie-Sanders-What-the-Hell-Have-You-Done-for-Us-Lately'>addressing exclusively progressive goals</a>, <a href='http://www.alternet.org/election-2016/bernie-gets-it-done-sanders-record-pushing-through-major-reforms-will-surprise-you'>than any other legislator</a>, by forging cross-party coalitions.<br><br>" +
         "He has <a href='http://www.nationaljournal.com/s/71225/bernie-sanders-is-loud-stubborn-socialist-republicans-like-him-anyway'>earned respect from Republicans</a> ranging from <a href='http://www.washingtonexaminer.com/mccain-calls-sanders-x-rated-but-an-honest-man/article/2577874'>John McCain</a> to the ultra-conservative <a href='http://www.bloomberg.com/features/2016-bernie-sanders-fundraising/'>Jim Inhofe</a>. If any Democratic president can reach across the aisle to work with a stubborn Republican Congress, it's Bernie Sanders."
 },{
     "id": "where-is-he-on-guns",
     "question": "... isn't he too weak on gun control?",
-    "answer": 
+    "answer":
         "Hardly. He has <a href='http://votesmart.org/candidate/evaluations/27110/bernie-sanders/37'>consistent high ratings</a> from the Brady Campaign to Prevent Gun Violence and a <a href='http://www.politifact.com/truth-o-meter/statements/2016/jan/20/bernie-s/bernie-sanders-nra-report-card-d-minus-most-recent/'>D- rating</a> from the NRA.<br><br>" +
         "His position is that <a href='http://thehill.com/blogs/blog-briefing-room/255741-sanders-sees-consensus-for-serious-gun-control'>a broad consensus of common-sense gun control measures</a> is possible if neither side is alienated through overly divisive rhetoric."
 },{
@@ -41,7 +43,7 @@ var faq = [{
 },{
     "id": "what-about-foreign-policy",
     "question": "... does he have any experience with foreign policy?",
-    "answer": 
+    "answer":
         "While Bernie's main focus is on domestic issues, he has shown remarkable foresight when it comes to foreign policy decisions. He was a member of a small minority voting against the War in Iraq in 2003, <a href='https://www.youtube.com/watch?v=NdFw1btbkLM'>arguing in an impassioned speech</a> that overthrowing Saddam Hussein would result in &quot;disaster&quot;. He was right.<br><br>" +
         "And Bernie is one of very few U.S. politicians to <a href='http://takingnote.blogs.nytimes.com/2016/03/22/straight-talk-from-bernie-sanders-on-the-israeli-palestinian-conflict/'>criticize the disproportionate nature of Israel's war in Gaza</a>, to <a href='http://www.commondreams.org/news/2016/04/17/bernie-sanders-us-cant-be-blackmailed-saudi-arabia'>bring attention to the U.S.'s questionable ties to Saudi Arabia</a>, and to <a href='https://topherhallquist.wordpress.com/2016/03/11/full-transcript-of-the-1985-sanders-interview-sampled-in-univisions-debate-with-some-commentary/'>point out the negative consequences of the U.S.'s Latin American interventions of the 1980s</a>.<br><br>" +
         "In fact, even Lawrence Korb, <i>Reagan</i>'s Assistant Defense Secretary, has praised Bernie's foreign policy philosophy <a href='http://www.politico.com/magazine/story/2016/02/bernie-sanders-foreign-poicy-213619'>as one of pragmatic realism</a>, comparing its emphasis on restraint and diplomacy to the foreign policy of Eisenhower."
@@ -52,7 +54,7 @@ var faq = [{
 },{
     "id": "can-he-get-minority-support",
     "question": "... aren't his supporters all young white men?",
-    "answer": 
+    "answer":
         "This is a false narrative that keeps getting pushed. In reality, Bernie has the support of <a href='http://www.usatoday.com/story/news/politics/elections/2016/01/14/bernie-sanders-hillary-clinton-women-millennials/78810110/'>50% of young women</a>, is viewed more favorably than Hillary by <a href='https://twitter.com/ZaidJilani/status/692818532212543488'>Hispanics</a> and <a href='http://www.gallup.com/opinion/polling-matters/187577/bernie-sanders-popular-candidate-among-asian-americans.aspx'>Asian-Americans</a> nationwide, and does better among <a href='http://www.dailykos.com/story/2015/10/13/1431846/-Bernie-Sanders-is-doing-better-among-African-Americans-than-among-whites-in-Pennsylvania'>African-Americans than whites</a> in Pennsylvania and some other states.<br><br>" +
         "And this is not surprising given Bernie's unwavering commitment to rights for all Americans throughout his life. He took part in <a href='https://www.youtube.com/watch?v=TPWcmPE5shc'>MLK's March on Washington in 1963</a>, <a href='http://www.vox.com/2015/9/9/9295867/bernie-sanders-gay-soldiers'>stood up for the rights of LGBT soldiers in 1995</a>, and has maintained a <a href='http://votesmart.org/candidate/evaluations/27110/bernie-sanders/75'>100% voting record with Planned Parenthood</a> for his entire career."
 },{
@@ -62,7 +64,7 @@ var faq = [{
 }, {
     "id": "I-am-still-unsure",
     "question": "Hmm, these are some good points, but I'm still unconvinced. Where can I learn more?",
-    "answer": 
+    "answer":
         "To learn more about Bernie Sanders's positions on more than 50 different issues, check out <a href='http://feelthebern.org'>FeelTheBern.org</a>.<br><br>" +
         "You can also watch Robert Reich challenge these critiques of Bernie much better than I can <a href='https://www.youtube.com/watch?v=VfIhonVoFSg'>in his recent video</a>."
 }];
